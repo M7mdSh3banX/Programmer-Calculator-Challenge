484eb1e6040a4e2e4e3d1866ba57c086dd3f14fa
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-11 06:25:51 +0200

    add the rest of converters functions

--- app/src/main/java/com/example/programmercalculatorchallenge/MainActivity.kt
+++ app/src/main/java/com/example/programmercalculatorchallenge/MainActivity.kt
@@ -55,4 +55,43 @@ class MainActivity : AppCompatActivity() {
 
         })
     }
+
+    private fun binaryConverter() {
+        decimalInputField.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+    }
+
+    private fun octalConverter() {
+        decimalInputField.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+    }
+
+    private fun hexadecimalConverter() {
+        decimalInputField.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+    }
 }
