c2102631663eea5b09c21a6d285d43f313469d14
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-11 05:55:21 +0200

    implement decimalConverter function

--- app/src/main/java/com/example/programmercalculatorchallenge/MainActivity.kt
+++ app/src/main/java/com/example/programmercalculatorchallenge/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.programmercalculatorchallenge
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
@@ -11,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binaryInputField: TextInputEditText
     private lateinit var octalInputField: TextInputEditText
     private lateinit var hexadecimalInputField: TextInputEditText
-    private lateinit var calculateButton: Button
     private lateinit var clearButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +23,13 @@ class MainActivity : AppCompatActivity() {
         binaryInputField = findViewById(R.id.binaryInputField)
         octalInputField = findViewById(R.id.octalInputField)
         hexadecimalInputField = findViewById(R.id.hexadecimalInputField)
-        calculateButton = findViewById(R.id.calculate_btn)
         clearButton = findViewById(R.id.clear_btn)
 
         clearButton.setOnClickListener {
             clearData()
         }
+
+        decimalConverter()
     }
 
     private fun clearData() {
@@ -35,5 +37,22 @@ class MainActivity : AppCompatActivity() {
         binaryInputField.text?.clear()
         octalInputField.text?.clear()
         hexadecimalInputField.text?.clear()
+    }
+
+    private fun decimalConverter() {
+        decimalInputField.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binaryInputField.setText(s.toString().toIntOrNull()?.toString(2))
+                octalInputField.setText(s.toString().toIntOrNull()?.toString(8))
+                hexadecimalInputField.setText(
+                    s.toString().toIntOrNull()?.toString(16)?.uppercase()
+                )
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
     }
 }
