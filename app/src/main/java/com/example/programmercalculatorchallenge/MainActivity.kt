f726dfa4247813bc61aa594e634432cc3c365cfe
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-03-12 16:01:11 +0200

    finish all functions implementation

--- app/src/main/java/com/example/programmercalculatorchallenge/MainActivity.kt
+++ app/src/main/java/com/example/programmercalculatorchallenge/MainActivity.kt
@@ -19,17 +19,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        decimalInputField = findViewById(R.id.decimalInputField)
-        binaryInputField = findViewById(R.id.binaryInputField)
-        octalInputField = findViewById(R.id.octalInputField)
-        hexadecimalInputField = findViewById(R.id.hexadecimalInputField)
-        clearButton = findViewById(R.id.clear_btn)
+        viewsId()
 
         clearButton.setOnClickListener {
             clearData()
         }
 
-        decimalConverter()
+        converters()
+    }
+
+    private fun viewsId() {
+        decimalInputField = findViewById(R.id.decimalInputField)
+        binaryInputField = findViewById(R.id.binaryInputField)
+        octalInputField = findViewById(R.id.octalInputField)
+        hexadecimalInputField = findViewById(R.id.hexadecimalInputField)
+        clearButton = findViewById(R.id.clear_btn)
     }
 
     private fun clearData() {
@@ -39,14 +43,23 @@ class MainActivity : AppCompatActivity() {
         hexadecimalInputField.text?.clear()
     }
 
+    private fun converters() {
+        decimalConverter()
+        binaryConverter()
+        octalConverter()
+        hexadecimalConverter()
+    }
+
     private fun decimalConverter() {
         decimalInputField.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binaryInputField.setText(s.toString().toIntOrNull()?.toString(2))
-                octalInputField.setText(s.toString().toIntOrNull()?.toString(8))
-                hexadecimalInputField.setText(
-                    s.toString().toIntOrNull()?.toString(16)?.uppercase()
-                )
+                if (decimalInputField.hasFocus()) {
+                    val decimal = s.toString().toLongOrNull(10) ?: return
+
+                    binaryInputField.setText(decimal.toString(2))
+                    octalInputField.setText(decimal.toString(8))
+                    hexadecimalInputField.setText(decimal.toString(16).uppercase())
+                }
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -57,9 +70,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun binaryConverter() {
-        decimalInputField.addTextChangedListener(object : TextWatcher {
+        binaryInputField.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binaryInputField.hasFocus()) {
+                    val binary = s.toString().toULongOrNull(2) ?: return
 
+                    decimalInputField.setText(binary.toString(10))
+                    octalInputField.setText(binary.toString(8))
+                    hexadecimalInputField.setText(binary.toString(16).uppercase())
+                }
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -70,9 +89,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun octalConverter() {
-        decimalInputField.addTextChangedListener(object : TextWatcher {
+        octalInputField.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (octalInputField.hasFocus()) {
+                    val octal = s.toString().toULongOrNull(8) ?: return
 
+                    decimalInputField.setText(octal.toString(10))
+                    binaryInputField.setText(octal.toString(2))
+                    hexadecimalInputField.setText(octal.toString(16).uppercase())
+                }
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -83,9 +108,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hexadecimalConverter() {
-        decimalInputField.addTextChangedListener(object : TextWatcher {
+        hexadecimalInputField.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (hexadecimalInputField.hasFocus()) {
+                    val hexadecimal = s.toString().toULongOrNull(16) ?: return
 
+                    decimalInputField.setText(hexadecimal.toString(10))
+                    binaryInputField.setText(hexadecimal.toString(2))
+                    octalInputField.setText(hexadecimal.toString(8))
+                }
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
